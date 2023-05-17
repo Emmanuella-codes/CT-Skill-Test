941764641e9c469fef583a0e9fd09bd12d4b76c2
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-05-17 23:40:37 +0100

    fix first and second hero contents

---
+++ script.js
@@ -0,0 +1,17 @@
+const sliderWrapper = document.querySelector(".slider-wrapper");
+const radioButtons = document.querySelectorAll(".radio-btn input");
+
+radioButtons.forEach((radioButton, index) => {
+  radioButton.addEventListener("click", () => {
+    // sliderWrapper.style.transform = `translateX(-${index * 400}px)`; // Adjust the translateX value based on the width of each slide
+    removeActiveClass();
+    sliderWrapper.children[index].classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  const activeItem = document.querySelector(".slider-item.active");
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
+}
